098a7378962a863fc6273b768d259184f231487a
Shrey008 <115262845+Shrey008@users.noreply.github.com>
2024-12-21 17:25:30 +0530

    Delete basic js directory

--- basic js/arrays/Arrays.js
+++
@@ -1,5 +0,0 @@
-
-//we can store more than one elements in array
-var users = ["harsh","shrey","noman","mikail"];
-var arr = [1,2,"shrey",23.5,function(){},[]];
-console.log(arr);  
--- basic js/variables.js
+++
@@ -1,7 +0,0 @@
-var a = 122; //var can be changed 
-var b = 55;
-
-const discount = 34; //constants cant be stored and changed
-
-
-//compiler and interpretr are translators hai jo ki english ko convert lar rahe machine code mein
