914c5c39dfa336df668dc55d7af039ea33c2b7c9
Shrey008 <115262845+Shrey008@users.noreply.github.com>
2024-12-21 17:27:50 +0530

    Add files via upload

---
+++ GSAP/scriot.js
@@ -0,0 +1,16 @@
+// gsap.to("#box",{
+//     x:850,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1 
+// })
+
+// gsap.from("#box",{
+//     x:850,
+//     rotate:360,
+//     backgroundColor:"blue",
+//     duration:2,
+//     delay:1 
+// })
+
---
+++ GSAP/script.js
@@ -0,0 +1,28 @@
+var tl = gsap.timeline()
+
+tl.to("#box1",{
+    x:600,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    delay:1
+ })
+
+
+ tl.to("#box2",{
+    x:600,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    
+ })
+
+ tl.to("#box3 ",{
+    x:600,
+    rotate:360,
+    scale:0.5,
+    duration:2,
+    
+ })
+
+
